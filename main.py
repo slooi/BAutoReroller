d94e54cc6078e29fd45c393f88ed12aeb7972bd9
s <s.looi2@outlook.com>
2025-07-27 13:01:34 +0900

    :sparkles:implement tap mirroring

--- main.py
+++ main.py
@@ -4,15 +4,32 @@ from PIL import Image, ImageTk
 import av
 import threading
 import random
+from icecream import ic
+import subprocess
 
 # --- Configuration ---
+# Set to your device serial if you have more than 1 device connected to adb. 
+# You can find by typing `adb devices`. Example serials: adb-RJV8-su22._adb-tls-connect._tcp, 192.168.1.2:25345
+DEVICE_SERIAL = "" 
+
 HOST = "127.0.0.1"
 PORT = 1234
+
+# CONSTANTS
 WINDOW_TITLE = "scrcpy Real-Time Stream (Low Latency)"
 
+def adb_tap(x, y,device_serial=DEVICE_SERIAL):
+    device_serial_cmd=""
+    if device_serial:
+        device_serial_cmd=f"-s {device_serial} " if device_serial else ""
+
+    cmd = f"adb {device_serial_cmd}shell input tap {x} {y}"
+    ic(x,y)
+    ic(cmd)
+    subprocess.run(cmd, check=True)
 
 class H264Renderer:
-    def __init__(self, root):
+    def __init__(self, root:tk.Tk):
         self.root = root
         self.root.title(WINDOW_TITLE)
         self.label = tk.Label(root)
@@ -26,12 +43,21 @@ class H264Renderer:
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
+        # Event handlers
+        self.label.bind("<Button-1>",self.click)
+
         # Start the network/decoder thread
         self.worker_thread = threading.Thread(target=self._network_and_decode_loop, daemon=True)
         self.worker_thread.start()
         
-        # 
+        # Render loop
         self.render_latest_frame()
+
+    def click(self,event):
+        threading.Thread(target=adb_tap,args=(event.x,event.y,),daemon=True).start()
+        ic(event)
+
+
 
     def on_close(self):
         """Handle window closing."""
@@ -103,8 +129,18 @@ class H264Renderer:
         self.root.after(33, self.render_latest_frame)
 
 
+# def setup_scrcpy_server():
+#     print("[*] Setting up srcpy server")
+#     subprocess.run("adb forward tcp:1234 localabstract:scrcpy",check=True)
+#     subprocess.run("adb shell CLASSPATH=/data/local/tmp/scrcpy-server-manual.jar app_process / com.genymobile.scrcpy.Server 3.3.1 tunnel_forward=true audio=false control=false cleanup=false raw_stream=true max_size=1920",check=True)
+#     print("[*] Scrcpy server setup complete")
+
 def main():
     # Run scrcpy: scrcpy --no-control --tcpip=1234
+    # t=threading.Thread(target=setup_scrcpy_server)
+    # t.start()
+    # t.join()
+
     root = tk.Tk()
     app = H264Renderer(root)
     root.mainloop()
